43ccd6021c8385960d1b3fc3eae9608be7e2093a
Brian <brinaldi000@gmail.com>
2019-12-29 00:45:15 -0500

    add

---
+++ 2Functions/introFunc.py
@@ -0,0 +1,6 @@
+def hello(name):
+    '''There is somehting that needs to get put here, but I don't really care'''
+    print("Hello "+name+"!")
+
+hello("Test")
+hello("There is nothing important here")
