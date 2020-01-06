3484e78aeb8285e4a8d75d801a63176d070ebb0b
unCl0ud <b.rinaldi@pm.me>
2020-01-05 20:56:30 -0500

    Finished Chapter 3

--- 2Functions/introFunc.py
+++
@@ -1,7 +0,0 @@
-def hello(name):
-    '''There is somehting that needs to get put here, but I don't really care'''
-    print("Hello "+name+"!")
-
-hello("Test")
-hello("There is nothing important here")
-print("Thiis is not calling the hello funtion")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # automateTheBoringStuff-Cloudy
 Learning Python from Automate the Boring Stuff
+
+Things in this repo are updated as I progress through the book.
+Items are sorted by chapter number and chapter title is shown next to the number
